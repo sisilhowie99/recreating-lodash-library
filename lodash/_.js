1c5675488e75dc7e78ff98686bc27239e15bb706
sisilhowie99 <sisil.anastasia@gmail.com>
2020-05-22 21:40:17 +1000

    finishes the first 10 steps

--- lodash/_.js
+++ lodash/_.js
@@ -1,4 +1,24 @@
+const _ = {
+    clamp(num, lower, upper) {
+        let lowerClampedValue = Math.max(num, lower);
+        let clampedValue = Math.min(lowerClampedValue, upper);
+        return clampedValue;
+        // alternatively can probably use if statements/switch cases
+    },
 
+    inRange(num, start, end) {
+        if(end === undefined) {
+            [end, start] = [start, 0];
+        };
+        
+        if(start > end) {
+            [end, start] = [start, end];
+        };
+
+        let isInRange = start <= num && num < end;
+        return isInRange;
+    }
+}
 
 
 
